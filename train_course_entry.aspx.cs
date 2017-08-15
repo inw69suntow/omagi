c8def3e072cf2a13269767cfc2d761bad2cdc143
yutthana <yutthana.saneha@ktb.co.th>
2017-08-15 16:57:09 +0700

    แก้ไข menu

--- train_course_entry.aspx.cs
+++ train_course_entry.aspx.cs
@@ -12,13 +12,14 @@ using System.Xml.Linq;
 using System.Data;
 using System.Data.Common;
 using System.Data.OleDb;
+using omagi_model;
+using System.Collections.Generic;
 
 public partial class _train_course_entry : System.Web.UI.Page
 {
     override protected void OnInit(EventArgs e)
     {
-        btnCreate.Attributes.Add("onclick",
-                  this.GetPostBackEventReference(btnCreate));
+        btnCreate.Attributes.Add("onclick",GetPostBackEventReference(btnCreate));
         base.OnInit(e);
     }
 
@@ -35,28 +36,28 @@ public partial class _train_course_entry : System.Web.UI.Page
 
         if (newVal.Value == "")
         {
-            Load_Table();
+            Load_Table(null);
             return;
         }
 
         try
         {
             string strConnString = ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString;
-            OleDbConnection con = new OleDbConnection(strConnString);                
+            OleDbConnection con = new OleDbConnection(strConnString);
             OleDbCommand objCmd = new OleDbCommand();
-            string newValStr=newVal.Value.ToString().Trim();
-            newVal.Value="";
-            string[] recstrSet=newValStr.Split(',');
+            string newValStr = newVal.Value.ToString().Trim();
+            newVal.Value = "";
+            string[] recstrSet = newValStr.Split(',');
             string sql = "DELETE from tb_train_course;";
 
-            foreach(string recStr in recstrSet)
+            foreach (string recStr in recstrSet)
             {
-                    if (recStr.Trim() != "")
-                    {
-                        sql += "INSERT INTO tb_train_Course ( ";
-                        sql += " fl_course ";
-                        sql += " ) VALUES ( ";
-                        sql += " '" + recStr.Trim().Replace("'", "''").Replace(";", "") + "'); ";
+                if (recStr.Trim() != "")
+                {
+                    sql += "INSERT INTO tb_train_Course ( ";
+                    sql += " fl_course ";
+                    sql += " ) VALUES ( ";
+                    sql += " '" + recStr.Trim().Replace("'", "''").Replace(";", "") + "'); ";
                 }
             }
 
@@ -67,16 +68,19 @@ public partial class _train_course_entry : System.Web.UI.Page
             sql += " '', ";
             sql += " '" + DateTime.Now.Year + DateTime.Now.Month.ToString().PadLeft(2, '0') + DateTime.Now.Day.ToString().PadLeft(2, '0') + DateTime.Now.Hour.ToString().PadLeft(2, '0') + DateTime.Now.Minute.ToString().PadLeft(2, '0') + DateTime.Now.Second.ToString().PadLeft(2, '0') + "', ";
             sql += " '" + Request.UserHostAddress + "'); ";
-    
+
             con.Open();
             objCmd.Connection = con;
             objCmd.CommandText = sql;
             objCmd.ExecuteNonQuery();
             con.Close();
-            Load_Table();
+            Load_Table(null);
         }
         catch (Exception ex) { }
     }
+
+
+
     protected void Page_Load(object sender, EventArgs e)
     {
         try
@@ -93,10 +97,13 @@ public partial class _train_course_entry : System.Web.UI.Page
             return;
         }
 
-        Load_Table();
+        Load_Table(null); 
     }
 
-    private void Load_Table()
+
+
+
+    private void Load_Table(String course)
     {
         dtGrid.Rows.Clear();
         dtGrid.Border = 1;
@@ -116,7 +123,12 @@ public partial class _train_course_entry : System.Web.UI.Page
             string strConnString = ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString;
             OleDbConnection con = new OleDbConnection(strConnString);
             con.Open();
-            string sql = "SELECT fl_course from tb_train_Course order by fl_course ";
+            string sql = "SELECT fl_course from tb_train_Course ";
+            if (course != null && !"".Equals(course))
+            {
+                sql += " where fl_course like '%"+course+"%'";
+            }
+            sql +=" order by fl_course ";
             OleDbDataReader dtReader;
             OleDbCommand objCmd = new OleDbCommand();
             objCmd.CommandText = sql;
@@ -166,5 +178,78 @@ public partial class _train_course_entry : System.Web.UI.Page
             maxJ.Value = count.ToString();
         }
         catch (Exception ex) { }
+    }
+
+
+
+    //private void Load_GvCourse()
+    //{
+    //    IList<Course> list = new List<Course>();
+    //    OleDbConnection con = null;
+    //    OleDbDataReader dtReader=null;
+    //    try
+    //    {
+    //        string strConnString = ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString;
+    //        con = new OleDbConnection(strConnString);
+    //        con.Open();
+    //        string sql = "SELECT fl_course from tb_train_Course order by fl_course ";
+
+    //        OleDbCommand objCmd = new OleDbCommand();
+    //        objCmd.CommandText = sql;
+    //        objCmd.Connection = con;
+    //        dtReader = objCmd.ExecuteReader();
+    //        while (dtReader.Read())
+    //        {
+    //            if (!dtReader.IsDBNull(0))
+    //            {
+    //                Course course = new Course();
+    //                course.Topic = dtReader.GetString(0);
+    //                list.Add(course);
+    //            }
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        throw ex;
+    //    }
+    //    finally
+    //    {
+    //        if (dtReader != null)
+    //        {
+    //            dtReader.Close();
+    //        }
+    //        if (con != null)
+    //        {
+                
+    //            con.Close();
+    //        }
+    //    }
+    //    gvTrainCourse.DataSource = list;
+    //    gvTrainCourse.DataBind();
+    //}
+
+
+    //protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+    //{
+    //    if (e.Row.RowType == DataControlRowType.DataRow)
+    //    {
+    //        TextBox txtTopic = new TextBox();
+    //        txtTopic.Width = 600;
+    //        txtTopic.MaxLength = 255;
+    //        txtTopic.ID = "txtTopic";
+    //        // txtTopic.Text = (e.Row.DataItem as DataRowView).Row["Topic"].ToString();
+    //         txtTopic.Text=(e.Row.DataItem as omagi_model.Course).Topic;
+    //        e.Row.Cells[1].Controls.Add(txtTopic);
+
+    
+    //    }
+    //}
+    protected void btnSearch_Click(object sender, ImageClickEventArgs e)
+    {
+        Load_Table(txtKeyword.Text.Trim());
+    }
+    protected void txtKeyword_TextChanged(object sender, EventArgs e)
+    {
+
     }
 }
