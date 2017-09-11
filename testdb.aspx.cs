5fe6c4f05a2303a1d7f437ebcb381556dff52781
inw69suntow <yutthana.saneha@ktb.co.th>
2017-09-11 22:42:18 +0700

    ccc

---
+++ testdb.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+
+public partial class testdb : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        OleDbConnection Conn =null;
+        try
+        {
+            Conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString);
+            OleDbCommand command = Conn.CreateCommand();
+            string sql = " SELECT distinct fl_user_id,fl_user_time from tb_logontime ";
+            Conn.Open();
+            command.CommandText = sql;
+            command.Connection = Conn;
+            // command.Parameters.Clear();
+            OleDbDataReader rs = command.ExecuteReader();
+            if (rs.Read())
+            {
+                lblResponse.Text = "This login is currently in-use";
+            }
+            rs.Close();
+            lblResponse.Text = "connection success " + Conn.ConnectionString;
+        }
+        catch (Exception ex)
+        {
+            lblResponse.Text = ex.Message;
+        }
+        finally
+        {
+            Conn.Close();
+        }
+    }
+}
